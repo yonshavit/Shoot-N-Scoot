16dc917b8e15add9cf92b9f021d96d3859527154
YONATAN-PC\YonatanUser <shayon@post.bgu.ac.il>
2019-04-14 16:52:58 +0300

    Finished Projectile 1.0 with all the tickets in Trello handled.

--- ShootNScoot/Assets/Scripts/Collision/Projectile.cs
+++ ShootNScoot/Assets/Scripts/Collision/Projectile.cs
@@ -3,28 +3,58 @@ using UnityEngine;
 
 namespace Assets.Scripts.Collision
 {
+    [RequireComponent(typeof(SpriteRenderer))]
     public class Projectile : MonoBehaviour
     {
         [SerializeField] private int index;
+        private SpriteRenderer mySpriteRenderer;
+        private float speed;
+        private Vector3 myOrigin;
+        private float distanceToHead;
+        private ProjectileManager manager;
 
-        public void InitProjectile(float orgOrientation)
+        public void InitProjectile(float orgOrientation, float speed, Vector3 origin, ProjectileManager manager)
         {
             transform.eulerAngles = new Vector3(0, 0, orgOrientation);
+            this.speed = speed;
+            this.myOrigin = transform.position;
+            this.distanceToHead = Vector3.Distance(origin, myOrigin);
+            this.manager = manager;
         }
 
         public void HandleAbsorption()
         {
-            throw new NotImplementedException();
+            // TODO Animation
+            manager.DecreaseProjectileCounter();
+            Destroy(gameObject);
         }
 
         public void HandleDeflection()
         {
-            throw new NotImplementedException();
+            index--;
+            if (index == 0)
+            {
+                manager.DecreaseProjectileCounter();
+                Destroy(gameObject);
+            }
+
+            mySpriteRenderer.sprite = manager.GetProjectileSprite(index);
+        }
+
+        private void Move()
+        {
+            transform.position += speed * transform.right * Time.deltaTime;
+            
+            if(!mySpriteRenderer.enabled && Vector3.Distance(transform.position, myOrigin) > distanceToHead)
+            {
+                mySpriteRenderer.enabled = true;
+            }
+            
         }
 
         void Start()
         {
-            // TODO complete
+            mySpriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         void Update()
--- ShootNScoot/Assets/Scripts/Collision/ProjectileManager.cs
+++ ShootNScoot/Assets/Scripts/Collision/ProjectileManager.cs
@@ -9,24 +9,39 @@ namespace Assets.Scripts.Collision
 {
     public class ProjectileManager : MonoBehaviour
     {
-        public int projectileCount;
+        [SerializeField] private int projectileCount;
+        public float speed;
+        [SerializeField] private Sprite proj1;
+        [SerializeField] private Sprite proj2;
+        [SerializeField] private Sprite proj3;
+        [SerializeField] private Sprite proj4;
+
+        public void DecreaseProjectileCounter()
+        {
+            if (projectileCount-- == 0)
+                Destroy(gameObject);
+        }
+
+        public Sprite GetProjectileSprite(int index)
+        {
+            switch (index)
+            {
+                case 1: return proj1;
+                case 2: return proj2;
+                case 3: return proj3;
+                case 4: return proj4;
+                default: return proj1;
+            }
+        }
 
         public void InitProjectiles(float orgOrientation)
         {
             var projectiles = GetComponentsInChildren<Projectile>();
             projectileCount = projectiles.Length;
-
+            var head_position = projectiles[0].transform.position;
             foreach (var projectile in projectiles)
             {
-                projectile.InitProjectile(orgOrientation);
-            }
-        }
-
-        void Update()
-        {
-            if (projectileCount == 0)
-            {
-                Destroy(gameObject);
+                projectile.InitProjectile(orgOrientation, speed, head_position, this);
             }
         }
     }
