436f53e7dfb6415fc2449522ecc6b19c3d2ea040
Shani Cohen <shanico91@gmail.com>
2019-04-15 13:36:30 +0300

    background camera

---
+++ ShootNScoot/Assets/Scripts/GameLogic/BackgroundManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.GameLogic
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class BackgroundManager : MonoBehaviour
+    {
+        private SpriteRenderer backgroundImage;
+        private Resolution resolution;
+
+        void Start()
+        {
+            backgroundImage = GetComponent<SpriteRenderer>();
+            resolution = Screen.currentResolution;
+            backgroundImage.transform.localScale = new Vector3(resolution.width, resolution.height, 1);
+        }
+
+        void Update()
+        {
+            if (resolution.width != Screen.currentResolution.width ||
+                resolution.height != Screen.currentResolution.height)
+            {
+                resolution = Screen.currentResolution;
+                backgroundImage.transform.localScale = new Vector3(resolution.width, resolution.height, 1);
+            }
+        }
+
+
+    }
+}
