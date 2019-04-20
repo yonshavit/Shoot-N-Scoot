0ddce86ac4c17d3bd9b78d3c80aa2c9384b4de06
Sagie <sagielevy21@gmail.com>
2019-04-20 14:42:41 +0300

    added comments in code

--- ShootNScoot/Assets/Scripts/GameLogic/AI/Turret.cs
+++ ShootNScoot/Assets/Scripts/GameLogic/AI/Turret.cs
@@ -17,7 +17,6 @@ namespace Assets.Scripts.GameLogic.AI
         [SerializeField] private float shotCooldownSeconds = 0.5f;
         [SerializeField] private float shotFireOffset = 1.5f;
 
-        //private SpriteRenderer turretSprite;
         private Animator anim;
         private float lastShotTime;
         private bool aiEnabled;
@@ -29,7 +28,6 @@ namespace Assets.Scripts.GameLogic.AI
 
         void Start()
         {
-            //turretSprite = GetComponent<SpriteRenderer>();
             anim = GetComponent<Animator>();
             lastShotTime = Time.time;
             aiEnabled = true;
@@ -42,11 +40,12 @@ namespace Assets.Scripts.GameLogic.AI
                 var targetDirection = (shooter.transform.position - transform.position).normalized;
                 var targetAngle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
 
+                // Rotate towards the shooter player
                 transform.rotation = Quaternion.AngleAxis(targetAngle, Vector3.forward);
 
                 if (Time.time - lastShotTime > shotCooldownSeconds)
                 {
-                    // Cooldown has passed, player may shoot again
+                    // Cooldown has passed, turret may shoot again
                     lastShotTime = Time.time;
 
                     // Create a new projectile and initiate it
@@ -55,6 +54,8 @@ namespace Assets.Scripts.GameLogic.AI
                         Quaternion.AngleAxis(targetAngle, Vector3.forward));
 
                     newProjectile.InitProjectiles();
+
+                    // Call shoot animation which will return to idle animation after a single animation cycle
                     anim.Play("Shoot");
                 }
             }
