dda60788bedfa135d12eed8aff71f028f20ffcc9
Shani Cohen <shanico91@gmail.com>
2019-04-25 16:03:48 +0300

    help screen is done

---
+++ ShootNScoot/Assets/Scripts/GameLogic/HelpScreenManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.GameLogic
+{
+    public class HelpScreenManager : MonoBehaviour
+    {
+
+        public void LoadByIndex(int sceneIndex)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+
+    }
+}
